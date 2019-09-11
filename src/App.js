f03a4cb46b13210402c4722a7bc36959657a6729
Laurence Dale <laurencedale1986@gmail.com>
2019-09-11 12:14:10 +0100

    updates

--- src/App.js
+++ src/App.js
@@ -11,7 +11,14 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          So this is just the React default app home page. Using this as an example for CI/CD integration with security tools. Once the pipeline is set up, I'll throw in some intentional vulnerabilities to demonstrate alerting capabilities and potential build-breaking problems.
+          <br></br><br></br>
+          Edit <code>src/App.js</code> and save to reload.
+          <br></br><br></br>
+          The pipeline should look as follows:
+          <br></br><br></br>
+          Git push > Jenkins Pipeline > Code Review > Burp Scan > Deploy to Artifactory/AppServer
+      
         </p>
       </div>
     );
